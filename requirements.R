99b685c09fbd64b38c884531047cce3e30edc91c
Utkarsh Saini <sainiayush24@gmail.com>
2026-01-22 05:34:54 +0000

    Add requirements.R to install and load libraries
    This script installs and loads essential libraries for data analysis and modeling in R.

---
+++ requirements.R
@@ -0,0 +1,15 @@
+# Install required libraries
+install.packages(c(
+  "tidyverse",
+  "modeldata",
+  "fitdistrplus",
+  "moments",
+  "caret"
+))
+
+# Load libraries
+library(tidyverse)
+library(modeldata)
+library(fitdistrplus)
+library(moments)
+library(caret)
